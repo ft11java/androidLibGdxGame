f0151b8bb46760bf357bf3d4f3cb1279baed6d53
ft11java <ft11java>
2023-12-07 01:32:41 +0300

    first commit

---
+++ android/src/com/taner/ninjafruitstarter/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.taner.ninjafruitstarter;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.taner.ninjafruitstarter.NinjaFruit;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new NinjaFruit(), config);
+	}
+}
